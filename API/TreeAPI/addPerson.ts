90c6d49be67745c5cbbad91fb4e458b41f55bc55
rdincher <rdincher14@gmail.com>
2021-02-09 23:01:15 -0600

    Create basic person type
    Add basic conversion from parameter to person with validation a name exists

--- API/TreeAPI/addPerson.ts
+++ API/TreeAPI/addPerson.ts
@@ -1,10 +1,32 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import {ResponseObject}  from "./types";
+import {ResponseObject, Person}  from "./types";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  let eventParameters = event.queryStringParameters;
+  try{
+    let newPerson = createPersonObject(eventParameters);
+  } catch(error) {
+    let errorMsg = error.message
+    return createResponse(400, errorMsg)
+  }
+
 
   return createResponse(200, 'Success');
 };
+
+const createPersonObject = (parameters:any): Person => {
+  if(!parameters.first_name){
+    throw Error("Person Requires first name")
+  }
+  if(!parameters.last_name){
+    throw Error("Person Requires last name")
+  }
+  let myPerson: Person = {
+    first_name: parameters.first_name,
+    last_name: parameters.last_name
+  }
+  return myPerson
+}
 
 const createResponse = (statusCode:number, body:string): ResponseObject => {
   return {
--- API/TreeAPI/types.ts
+++ API/TreeAPI/types.ts
@@ -1,6 +1,17 @@
-interface ResponseObject {
+export interface ResponseObject {
     statusCode: number
     body: string
 }
 
-export { ResponseObject }
+export interface Person {
+    first_name: string
+    nickname?: string
+    last_name: string
+    middle_name?: string
+    born?: Date
+    died?: Date
+    details?: string
+    parents?: Array<number>
+    children?: Array<number>
+    siblings?: Array<number>
+}
